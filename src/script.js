1334791e98559b5c05aa7128b6283ff2f01208c2
rogerc091 <rogerc091@gmail.com>
2024-10-22 13:35:58 +0200

    Adicionando toda estrutura do script

--- src/script.js
+++ src/script.js
@@ -3,6 +3,40 @@
 
 //ESCREVA SEU CÓDIGO AQUI!!
 
+function Calculadora(n1,n2,operador){
+  let numero1 = parseFloat(n1);
+  let numero2 = parseFloat(n2);
+  
+  let soma = 0 ;
+  if(operador === "+"){
+    soma = numero1 + numero2;
+  }
+  if(operador === "-"){
+    soma = n1 - n2;
+  }
+  if(operador === "*"){
+    soma = n1 * n2;
+  }
+  if(operador === "/"){
+    soma = n1 / n2;
+  }
+  if(operador === "e"){
+    let contador = 1;
+    soma = n1;
+    while(contador < n2){
+      
+      soma = soma * n1;
+      console.log(parseFloat(soma))
+      contador++;
+    }
+    if(soma > 1000000 || soma === undefined){
+      soma = "ERROR";
+    }
+  }
+  return soma
+  
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
